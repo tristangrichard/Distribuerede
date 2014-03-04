36d97505b9ff35eb4c06eca6229a4f836b48a94b
Emasb <asbech.emil@gmail.com>
2014-03-04 09:50:24 +0100

    Kan nu sende kommandoer til serveren

--- Clients/src/clients/Remoteclient.java
+++ Clients/src/clients/Remoteclient.java
@@ -3,34 +3,50 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Remoteclient {
 	public static void main(String[] args) throws IOException {
 		float avg;
 		byte b;
+		String temp;
+		char msg = 'a';
 		DataOutputStream out = null;
 		DataInputStream in = null;
 		Socket s = null;
+		Scanner scan = null;
 		try {
-			s = new Socket("10.16.163.98", 6005);
+			System.out.println("Skriv serverens IP adresse.");
+			scan = new Scanner(System.in);
+			temp = scan.next();
+			s = new Socket(temp, 6005);
 			out = new DataOutputStream(s.getOutputStream());
 			in = new DataInputStream(s.getInputStream());
-			try {
-				b = (byte)'g';
-				out.write(b);
-				out.flush();
-				avg = in.read();
-				avg = ((avg +140) /10);
-				System.out.println("Average temperatur: " + avg);
-			} catch(IOException e){
-				e.printStackTrace();
+			while(msg != 's' && msg != 'q'){
+				System.out.println("Klar til at sende kommando til server...");
+				temp = scan.next();
+				msg = temp.charAt(0);
+				try {
+					b = (byte)msg;
+					out.write(b);
+					out.flush();
+					if (msg == 'g'){
+						avg = in.read();
+						avg = ((avg +140) /10);
+						System.out.println("Average temperatur: " + avg);
+					}
+				} catch(IOException e){
+					e.printStackTrace();
+				}
 			}
 		} finally {
 			out.close();
 			in.close();
 			s.close();
+			scan.close();
 		}
+
 	}
 }
 
